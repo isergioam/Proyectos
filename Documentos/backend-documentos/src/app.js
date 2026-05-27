185f9f203fe92fa9b9fd420249a21a6fad368e30
Sergio Aguilar <isergioam@gmail.com>
2026-05-27 14:19:35 +0200

    feat: initialize express project structure with core dependencies and server configuration

--- Documentos/backend-documentos/src/app.js
+++ Documentos/backend-documentos/src/app.js
@@ -1,8 +1,8 @@
 import express from 'express'
 import cors from 'cors'
-import authRoutes from './routes/auth.routes.js'
+//import authRoutes from './routes/auth.routes.js'
 import documentsRoutes from './routes/documents.routes.js'
-import healthRoutes from './routes/health.routes.js'
+//import healthRoutes from './routes/health.routes.js'
 import { notFound } from './middlewares/notFound.middleware.js'
 import { errorHandler } from './middlewares/error.middleware.js'
 
@@ -15,8 +15,8 @@ app.use(cors({
 app.use(express.json())
 app.use('/uploads', express.static('src/uploads'))
 
-app.use('/api', healthRoutes)
-app.use('/api', authRoutes)
+//app.use('/api', healthRoutes)
+//app.use('/api', authRoutes)
 app.use('/api', documentsRoutes)
 
 app.use(notFound)
